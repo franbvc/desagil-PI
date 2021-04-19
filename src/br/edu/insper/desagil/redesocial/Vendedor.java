ad0b8f6cb9c47b7ffbe2b867f1de61c743101ebb
franbvc <franbvc@gmail.com>
2021-04-19 14:28:38 -0300

    cria classes e test case

---
+++ src/br/edu/insper/desagil/redesocial/Vendedor.java
@@ -0,0 +1,5 @@
+package br.edu.insper.desagil.redesocial;
+
+public class Vendedor {
+
+}
---
+++ test/br/edu/insper/desagil/redesocial/VideoTest.java
@@ -0,0 +1,14 @@
+package br.edu.insper.desagil.redesocial;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VideoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
